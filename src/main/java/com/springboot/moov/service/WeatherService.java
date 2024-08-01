58a569fa25fa7abda48f2bc022a8f9806697103c
Playdata <kimheesu98@gmail.com>
2024-08-01 15:07:08 +0900

    weather

--- src/main/java/com/springboot/moov/service/WeatherService.java
+++ src/main/java/com/springboot/moov/service/WeatherService.java
@@ -1,8 +1,9 @@
 package com.springboot.moov.service;
 
 import com.springboot.moov.data.response.WeatherResponse;
+import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
-
+@Service
 public interface WeatherService {
     Mono<WeatherResponse> getCurrentWeather(double lat, double lon);
 }
